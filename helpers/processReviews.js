23c0982f92acb4ed46f1a94c81be6a36a6f1bcab
Reynaldo Reyes <reynaldo.reyes@wowcher.co.uk>
2021-12-24 09:30:46 +0000

    Refactor more code

--- helpers/processReviews.js
+++ helpers/processReviews.js
@@ -1,5 +1,16 @@
-const processReviews = (reviews) => {
-  let [five, four, three, two, one] = Array(5).fill(0);
+const getAverage = (reviews) =>
+  reviews
+    .map((review) => review.rating)
+    .reduce((prev, curr) => prev + curr, 0) / reviews.length;
+
+const getResults = (reviews) => {
+  const [five, four, three, two, one] = Array(5).fill(0);
+
+  /*
+   * I did not use a reduce array on this instance due to the fact
+   *  if any rating is equal to 0 there would be no entry on the
+   *  produced array meaning there would be no data for the bar chart.
+   */
 
   reviews.map((review) => {
     if (review.rating === 5) five++;
@@ -8,14 +19,15 @@ const processReviews = (reviews) => {
     if (review.rating === 2) two++;
     if (review.rating === 1) one++;
   });
+  return [five, four, three, two, one];
+};
 
-  const average =
-    reviews
-      .map((review) => review.rating)
-      .reduce((prev, curr) => prev + curr, 0) / reviews.length;
-  const count = reviews.length;
-  const results = [five, four, three, two, one];
-  return { average, results, count };
+const processReviews = (reviews) => {
+  return {
+    average: getAverage(reviews),
+    results: getResults(reviews),
+    count: reviews.length,
+  };
 };
 
 export default processReviews;
